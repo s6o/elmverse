b38fdb179fc00994019cafbf3cbebbc70828af1f
Oliver Sõro <oliver.s6ro@gmail.com>
2019-03-14 02:21:25 +0200

    Add sqlitex and httpoison dependencies to mix.exs.

--- mix.exs
+++ mix.exs
@@ -37,8 +37,10 @@ defmodule Elmverse.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:jason, "~> 1.1"},
+      {:plug_cowboy, "~> 2.0"},
+      {:sqlitex, "~> 1.5"},
+      {:httpoison, "~> 1.5"}
     ]
   end
 end
