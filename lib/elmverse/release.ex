9fc5050645ae81d507a13f7fa1b95caa5db0271a
Oliver Sõro <oliver.s6ro@gmail.com>
2019-03-14 23:13:50 +0200

    Add save/1 to Elmverse.Release module.

--- lib/elmverse/release.ex
+++ lib/elmverse/release.ex
@@ -12,4 +12,29 @@ defmodule Elmverse.Release do
     :released,
     :repo_id
   ]
+
+  alias __MODULE__
+  alias Sqlitex.Server, as: Db
+
+  @spec save(Release.t()) :: {:ok, Release.t()} | [{:error, atom()}]
+  def save(%Release{} = r) do
+    query = """
+      INSERT INTO package_release (pub_name, pkg_ver, released, repo_id)
+        VALUES ($1, $2, $3, $4)
+        ON CONFLICT (pub_name, pkg_ver) DO UPDATE SET
+          released = $3, repo_id = $4
+    """
+
+    with {:ok, _} <-
+           Db.query(:elmverse, query,
+             bind: [
+               r.pub_name,
+               r.pkg_ver,
+               r.released,
+               r.repo_id
+             ]
+           ) do
+      {:ok, r}
+    end
+  end
 end
