b0235973208d78c260d09b2f875f1d2a23afb3b6
Oliver Sõro <oliver.s6ro@gmail.com>
2019-03-28 00:11:17 +0200

    Started work on the repository view.

--- lib/elmverse_web/controllers/repository_controller.ex
+++ lib/elmverse_web/controllers/repository_controller.ex
@@ -1,8 +1,18 @@
 defmodule ElmverseWeb.RepositoryController do
   use ElmverseWeb, :controller
 
+  alias Elmverse.Repository.PackageSummary
+
   def index(conn, %{"elm_ver" => elm_ver}) do
-    render(conn, "repository.html", elm_ver: elm_ver)
+    repo_packages =
+      with {:ok, pkg_list} <- PackageSummary.list(elm_ver) do
+        pkg_list
+      else
+        _ ->
+          []
+      end
+
+    render(conn, "repository.html", elm_ver: elm_ver, repo_packages: repo_packages)
   end
 
   def show(conn, %{"elm_ver" => elm_ver, "pub" => pub, "pkg" => pkg, "ver" => ver}) do
--- lib/elmverse_web/views/repository_view.ex
+++ lib/elmverse_web/views/repository_view.ex
@@ -1,3 +1,9 @@
 defmodule ElmverseWeb.RepositoryView do
   use ElmverseWeb, :view
+
+  def release_date(unix_epoch) do
+    unix_epoch
+    |> DateTime.from_unix!(:second)
+    |> DateTime.to_string()
+  end
 end
